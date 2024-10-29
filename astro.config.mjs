a67a80f5d52fc98f39f9802c2958b0eeeb7ca424
James Bohrman <james@getoasis.io>
2024-10-29 19:26:55 -0400

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify/functions";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import simpleStackForm from "simple-stack-form";
@@ -28,16 +28,12 @@ export default defineConfig({
     simpleStackForm(),
   ],
   output: "hybrid",
-  adapter: netlify(),
+  adapter: netlify({
+    edgeMiddleware: true // Enable edge middleware
+  }),
   vite: {
     ssr: {
-      noExternal: ['@supabase/supabase-js', 'discord.js', 'color.js'],
-      external: ['@amplitude/analytics-node']
-    },
-    build: {
-      rollupOptions: {
-        external: ['@amplitude/analytics-node']
-      }
+      noExternal: ['@supabase/supabase-js', 'discord.js', 'color.js']
     }
   }
 });
