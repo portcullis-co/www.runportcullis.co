ec7737378d2290d14ac92bf36d08a9300aec3ccf
James Bohrman <james@getoasis.io>
2024-10-29 20:05:01 -0400

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import db from "@astrojs/db";
 import mdx from "@astrojs/mdx";
-import netlify from "@astrojs/netlify";
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
@@ -8,20 +8,20 @@ import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import simpleStackForm from "simple-stack-form";
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.runportcullis.co",
-  integrations: [mdx({
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "github-dark-dimmed"
-    },
-    gfm: true
-  }), icon(), sitemap(), react(), tailwind({
-    applyBaseStyles: false
-  }), simpleStackForm()],
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "github-dark-dimmed" },
+      gfm: true,
+    }),
+    icon(),
+    sitemap(),
+    react(),
+    tailwind({ applyBaseStyles: false }),
+    simpleStackForm(),
+  ],
   output: "server",
   adapter: cloudflare()
 });
