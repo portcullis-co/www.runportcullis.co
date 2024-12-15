3e8e9fc220d4ad866c0444df80442fce468483ac
James Bohrman <james@getoasis.io>
2024-12-15 15:10:51 -0500

    rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,9 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { siteConfig } from '@/config/site';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
@@ -8,9 +11,13 @@ export async function GET(context) {
 		title: siteConfig.name,
 		description: siteConfig.description,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
+		items: blog.map((post) => ({
 			link: `/blog/${post.slug}/`,
-		})),
+			// Note: this will not process components or JSX expressions in MDX files.
+			content: sanitizeHtml(parser.render(post.body), {
+			  allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+			}),
+			...post.data,
+		  })),
 	});
 }
